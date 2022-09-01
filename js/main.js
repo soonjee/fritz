133e4f086a8505b126cf3ee481701bd6366cf568
seungji Choi <youinxxs@naver.com>
2022-09-02 04:08:43 +0900

    20220902

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ $(function () {
     })
 
     var swiper = new Swiper(".mySwiper", {
-        loop: true,
+        loop: false,
         spaceBetween: 10,
         slidesPerView: 5,
-        freeMode: true,
-        watchSlidesProgress: true,
+        freeMode: false,
+        // watchSlidesProgress: true,
     });
     var swiper2 = new Swiper(".mySwiper2", {
-        loop: true,
+        loop: false,
         spaceBetween: 10,
         navigation: {
             nextEl: ".swiper-button-next",
